31867a7845f789c72b1bfbf4d14859ec5c67364f
ashish22cse17 <ashishcpr697@gmail.com>
2024-07-30 02:27:43 +0530

    dectructor bh

--- destructor2.cpp
+++ destructor2.cpp
@@ -20,7 +20,7 @@ int main(){
     cout<<"We are inside our main function"<<endl;
     cout<<"Creating first object n1"<<endl;
     num n1;
-    {
+    { 
         cout<<"Entering this block"<<endl;
         cout<<"Creating two more objects"<<endl;
         num n2, n3;
